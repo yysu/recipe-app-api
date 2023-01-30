66d5d1b8cd3bc90951f7ee7bbbf13e8aa411d65a
YYSU <jim123820@gmail.com>
2023-01-30 15:55:49 +0800

    fix flake8 error

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,7 +23,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs',
-    ),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
 ]
