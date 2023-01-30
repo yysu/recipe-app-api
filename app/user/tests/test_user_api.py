def1e144cd86ac3560deff61d549b3288ef465af
YYSU <jim123820@gmail.com>
2023-01-30 18:21:25 +0800

    fix flake8 issue

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,6 +11,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
+
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
